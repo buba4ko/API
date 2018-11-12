e9ba85d3561ea0f5ec11e2af4828e0fe1a320915
buba4ko <trendo@abv.bg>
2018-11-12 11:48:24 +0200

    init version for BookLibrary

---
+++ BookLibrary/BookLibrary/Startup.cs
@@ -0,0 +1,51 @@
+﻿using BookLibrary.Business;
+using BookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookLibrary
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            using (BookService service = new BookService())
+            {
+                List<Book> books = service.GetAll();
+
+                Book book = books.FirstOrDefault();
+
+                book.Quantity++;
+                service.EditBook(book);
+
+                Book book2 = service.GetBookByID(book.ID);
+
+                Book newBook = new Book {
+                    Author = "Mecho Puh",
+                    Titile = "прасчо и аз",
+                    Description = "дълго е за разказване",
+                    Genre = "приказки",
+                    Quantity = 1,
+                    CreatedDate = DateTime.Now
+                };
+
+                service.AddBook(newBook);
+
+
+                var booksByAuthor = service.GetByAuthor("Puh");
+                if (booksByAuthor.Count > 0)
+                {
+                    int bookID = booksByAuthor.First().ID;
+                    service.DeleteBook(bookID);
+                }
+
+
+            }
+
+            LibraryContext context = new LibraryContext();
+            var x = context.Books.ToList();
+        }
+    }
+}
